1dc400329f7024aa99fbc4e880aeb8a0ad68ded0
Ilya Starodubov <stil3113@mail.ru>
2013-11-19 19:58:09 +0600

    Lab3 - News: Last version

--- lab3/news/models.py
+++ lab3/news/models.py
@@ -3,3 +3,5 @@ from django.db import models
 # Create your models here.
 class News(models.Model):
     title = models.CharField(verbose_name=u'Title', max_length=255)
+    datetime = models.DateTimeField(verbose_name='Date of editing')
+    comment = models.TextField(verbose_name='Comment')
